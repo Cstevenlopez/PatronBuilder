bf3d4f0079988f91ebe8983023200b91aed9da87
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-10-16 11:22:05 -0600

    Creacion de la clase cocinero como el dorector del patron

--- src/main/java/org/steven/Cocinero.java
+++ src/main/java/org/steven/Cocinero.java
@@ -1,2 +1,24 @@
-package org.steven;public class Cocinero {
+package org.steven;
+
+public class Cocinero {
+    private PizzaBuilder pizzaBuilder;
+
+    public void setPizzaBuilder(PizzaBuilder pb)
+    {
+        pizzaBuilder = pb;
+    }
+
+    public Pizza getPizza()
+    {
+        return  pizzaBuilder.getPizza();
+    }
+
+    public void crearPizza()
+    {
+        pizzaBuilder.crearNuevaPizza();
+        pizzaBuilder.ponerNombre();
+        pizzaBuilder.crearMasa();
+        pizzaBuilder.crearSalsa();
+        pizzaBuilder.crearIngredientes();
+    }
 }
