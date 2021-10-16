996de68489460db7ea78a9836141c554785f8e96
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-10-16 11:23:56 -0600

    Clases concretas de pizzas que implementan la clase adstracta de PizzaBuilder

--- src/main/java/org/steven/BarbacoaPizzaBuilder.java
+++ src/main/java/org/steven/BarbacoaPizzaBuilder.java
@@ -1,2 +1,23 @@
-package org.steven;public class BarbacoaPizzaBuilder {
+package org.steven;
+
+public class BarbacoaPizzaBuilder extends PizzaBuilder{
+    @Override
+    public void ponerNombre() {
+        pizza.setNombre("Barbacoa");
+    }
+
+    @Override
+    public void crearMasa() {
+        pizza.setMasa("gorda");
+    }
+
+    @Override
+    public void crearSalsa() {
+        pizza.setSalsa("barbacoa");
+    }
+
+    @Override
+    public void crearIngredientes() {
+        pizza.setIngredientes("mozzarella, ternera, cebolla, maiz");
+    }
 }
--- src/main/java/org/steven/CarbonaraPizzaBuilder.java
+++ src/main/java/org/steven/CarbonaraPizzaBuilder.java
@@ -1,2 +1,24 @@
-package org.steven;public class CarbonaraPizzaBuilder {
+package org.steven;
+
+public class CarbonaraPizzaBuilder extends PizzaBuilder{
+
+    @Override
+    public void ponerNombre() {
+        pizza.setNombre("Carbonara");
+    }
+
+    @Override
+    public void crearMasa() {
+        pizza.setMasa("fina");
+    }
+
+    @Override
+    public void crearSalsa() {
+        pizza.setSalsa("carbonara");
+    }
+
+    @Override
+    public void crearIngredientes() {
+        pizza.setIngredientes("mozzarella, bacon, cebolla");
+    }
 }
