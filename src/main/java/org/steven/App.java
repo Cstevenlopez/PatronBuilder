19c4c940972b790e30b4b1c02650c91f3a06dc8f
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-10-16 11:43:33 -0600

    Ejecucion del ejercicio

--- src/main/java/org/steven/App.java
+++ src/main/java/org/steven/App.java
@@ -8,6 +8,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Pizza pizza;
+        Cocinero cocinero = new Cocinero();
+        PizzaBuilder bpb = new BarbacoaPizzaBuilder();
+        PizzaBuilder cpb = new CarbonaraPizzaBuilder();
+
+        cocinero.setPizzaBuilder(bpb);
+        cocinero.crearPizza();
+        pizza = cocinero.getPizza();
+        System.out.println(pizza.toString());
+
+        cocinero.setPizzaBuilder(cpb);
+        cocinero.crearPizza();
+        pizza = cocinero.getPizza();
+        System.out.println(pizza.toString());
     }
 }
